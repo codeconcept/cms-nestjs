a850f7509e1614e0043d0341c77001195229b61b
codeconcept <contact@codeconcept.fr>
2019-08-29 15:22:06 +0200

    using provided enableCors() method

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,17 +7,15 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 import config from './config';
-import { CustomCorsMiddleware } from './custom-cors.middleware';
 
 @Module({
-  imports: [MongooseModule.forRoot(config.mongoUri, { useNewUrlParser: true }), ArticlesModule, AuthModule, UsersModule],
+  imports: [
+    MongooseModule.forRoot(config.mongoUri, { useNewUrlParser: true }),
+    ArticlesModule,
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(CustomCorsMiddleware)
-      .forRoutes('articles');
-  }
-}
+export class AppModule {}
--- src/custom-cors.middleware.ts
+++ src/custom-cors.middleware.ts
@@ -5,11 +5,12 @@ export class CustomCorsMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
     // tslint:disable-next-line: no-console
     console.log('yeah, you entered CustomCorsMiddleware');
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept',
     );
+
     next();
   }
 }
